808046eb42a2e4d2e083a6471b4db034ca399902
jack <mgmt@sunspla.sh>
2021-06-19 14:08:49 -0400

    Changed hostname container to location container which should fetch geolocation. Removed hostname route info.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ async function loadText(url, event){
     event.target.querySelector('.gradient-border-container__inner__info__load').classList.add('done');
     event.target.querySelector('.gradient-border-container__inner__info__text-reload').classList.add('fail');
     event.target.querySelector('.gradient-border-container__inner__info__btn-reload').classList.add('fail');
-    console.log('something went wrong while attempting to fetch and load the text: ', error);
+    console.log('something went wrong while trying to fetch and load the text: ', error);
   }
 }
 
@@ -40,7 +40,7 @@ function copyTextToClipboard(event){
 }
 
 document.querySelector('#ip-container').addEventListener('fetching', loadText.bind(this, 'http://localhost:3001/text'));
-document.querySelector('#hostname-container').addEventListener('fetching', loadText.bind(this, 'http://localhost:3001/hn-text'));
+document.querySelector('#location-container').addEventListener('fetching', loadText.bind(this, 'http://localhost:3001/geo'));
 
 //bind the retryLoading funtion to the retry buttons
 document.querySelectorAll('.gradient-border-container__inner__info__btn-reload').forEach(elem => elem.addEventListener('click', tryLoading))
